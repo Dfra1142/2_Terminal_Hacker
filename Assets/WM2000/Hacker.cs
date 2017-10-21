296d84e00cf2d180fd7c44bf223e61659a6f0407
Dawson <dawsonfrakes123@gmail.com>
2017-10-20 21:24:36 -0400

    Creating a Win Screen

--- Assets/WM2000/Hacker.cs
+++ Assets/WM2000/Hacker.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
-#region ints/enums
-    // Game state
+    string[] level1Passwords = { "books", "aisle", "self", "password", "font", "borrow" };
+    string[] level2Passwords = { "prisoner", "handcuffs", "uniform", "arrest" };
+
+    #region Game state
     int level;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
+    string password;
 #endregion
 
     void Start()
     {
         ShowMainMenu();
-        print("Game has started");
     }
 
     void ShowMainMenu()
     {
         currentScreen = Screen.MainMenu;
+        password = "";
         Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local library");
@@ -38,24 +38,32 @@ public class Hacker : MonoBehaviour
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
 
     }
 
-    private void RunMainMenu(string input)
+    void RunMainMenu(string input)
     {
-        if (input == "1")
+        bool isValidLevelNumber = (input == "1" || input == "2");
+        if (isValidLevelNumber)
         {
-            level = 1;
-            StartGame();
-        }
-        else if (input == "2")
-        {
-            level = 2;
+            level = int.Parse(input);
             StartGame();
         }
         else if (input == "007")
         {
             Terminal.WriteLine("Please input a valid response Mr. Bond!");
+        }
+        else if (input == "69")
+        {
+            Terminal.WriteLine("A real response you dirty man");
+        }
+        else if (input == "420")
+        {
+            Terminal.WriteLine("Blaze in a real response kthx");
         }
         else
         {
@@ -66,8 +74,63 @@ public class Hacker : MonoBehaviour
     void StartGame()
     {
         currentScreen = Screen.Password;
-        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.ClearScreen();
+        switch(level)
+        {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number");
+                break;
+        }
         Terminal.WriteLine("Please input your password:");
-        print("User selected level " + level);
+    }
+
+    private void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            DisplayWinScreen();
+        }
+        else
+        {
+            Terminal.WriteLine("Incorrect password. Try again: ");
+        }
+    }
+
+    void DisplayWinScreen()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward()
+    {
+        switch (level)
+        {
+            case 1:
+                Terminal.WriteLine("Have a book!");
+                Terminal.WriteLine(@"
+    _______
+   /     //
+  /     //
+ /____ //
+(_____(/
+"               );
+                break;
+            case 2:
+                Terminal.WriteLine("You got the prison key!");
+                Terminal.WriteLine(@"
+ __
+/0 \_______
+\__/-=' = '
+"               );
+                break;
+        }
     }
 }
